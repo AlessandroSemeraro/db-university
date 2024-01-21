0939a49089c5168584e92c0b7c0962e5986a4418
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-01-22 00:09:10 +0100

    added screen DB

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>db-university</title>
+</head>
+<body>
+    <h1>DB-university</h1>
+    <img src="./img/db-university.jpg" alt="db-university">
+</body>
+</html>
